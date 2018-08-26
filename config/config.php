f442ba7bdaa7e698662202566b37c2b79bbdc054
Lodhel <lodhel@yahoo.com>
2018-08-26 21:36:06 +0300

    добавлен шаблонизатор Smarty

--- config/config.php
+++ config/config.php
@@ -6,8 +6,32 @@
  * Time: 20:30
  */
 
-    //< константы для обрпщения к контроллерам
+    //> константы для обрпщения к контроллерам
 
     define('PathPrefix', '../controllers/');
     define('PathPostfix', 'Controller.php');
-    //>
+    //<
+
+    //> используемый шаблон
+
+    $template = 'default' ;
+
+    // пути к файлам шаблонов (*.tpl)
+
+    define('TemplatePrefix', "../views/($template)/"); // где находятся шаблоны
+    define('TemplatePostfix', ".tpl"); // чтобы не дописывать каждый раз формат
+
+    // пути к файлам шаблонов в веб-пространстве
+    define('TemplateNamePath', "/templates/($template)/");
+    //<
+
+    //инициализация шаблона Smarty
+    require('../library/Smarty/libs/Smarty.class.php');
+    $smarty = new Smarty();
+
+    $smarty->setTemplateDir(TemplatePrefix);
+    $smarty->setCompileDir('../tmp/smarty/templates_c');
+    $smarty->setCaheDir('../tmp/smarty/cashe');
+    $smarty->setConfigDir('../library/Smarty/configs');
+
+    $smarty->assign('templateWebPath', TemplateNamePath);
